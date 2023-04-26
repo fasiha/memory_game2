e23c0bb256b20798c78d44fccd8f0c17461db63f
Ahmed Fasih <wuzzyview@gmail.com>
2023-04-25 19:38:12 -0700

    prettier ONLY

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,45 +1,41 @@
 import { Link, useNavigate } from "react-router-dom";
 import "./topbar.css";
-import React, { useState, useEffect } from 'react';
-import astro from '../../images/back-r/astronaut.png'
-import blue from '../../images/back-r/blue2.png'
+import React, { useState, useEffect } from "react";
+import astro from "../../images/back-r/astronaut.png";
+import blue from "../../images/back-r/blue2.png";
 
 // this way of writing might be able to get tested.
-// need to find out the way to make it testable 
+// need to find out the way to make it testable
 export default function TopBar(props) {
-    
-    const {green,setGreen} = props
-    function handleClick(){
-      setGreen(prev=>!prev)
-    }
-    return (
-      <div className="top">
-         <div className="topLeft" >
-         <h1 className='logo'>Memory</h1> 
-         </div>
+  const { green, setGreen } = props;
+  function handleClick() {
+    setGreen((prev) => !prev);
+  }
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <h1 className="logo">Memory</h1>
+      </div>
       <div className="topCenter">
         <ul className="topList">
-        <li className='topListItem'>
-               <Link to='/'>
-                 Home
-               </Link>
-        </li>
-        <li className='topListItem'>
-               <Link to='/about'>
-                 About
-               </Link>
-        </li>
+          <li className="topListItem">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="topListItem">
+            <Link to="/about">About</Link>
+          </li>
         </ul>
       </div>
       <div className="topRight">
         <ul className="topList">
-            <img 
-            src={green? blue:astro} data-testid={green? 'initial':'clicked'} className='topImg' onClick={handleClick}/> 
+          <img
+            src={green ? blue : astro}
+            data-testid={green ? "initial" : "clicked"}
+            className="topImg"
+            onClick={handleClick}
+          />
         </ul>
       </div>
-    </div>)
-      }
-
-
-
-  
+    </div>
+  );
+}
--- src/components/topbar/TopBar.test.js
+++ src/components/topbar/TopBar.test.js
@@ -1,19 +1,18 @@
-import {render, screen,fireEvent} from "@testing-library/react"
-import TopBar from './TopBar'
+import { render, screen, fireEvent } from "@testing-library/react";
+import TopBar from "./TopBar";
 import userEvent from "@testing-library/user-event";
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter } from "react-router-dom";
 import App from "../../App";
-import React, {useState as useStateMock} from 'react';
-
+import React, { useState as useStateMock } from "react";
 
 describe("App component", () => {
-    it("should render the logo and links", () => {
-      render(
+  it("should render the logo and links", () => {
+    render(
       <MemoryRouter>
         <TopBar />
       </MemoryRouter>
-      );
-      const logo = screen.getByText(/memory/i);
+    );
+    const logo = screen.getByText(/memory/i);
     expect(logo).toBeInTheDocument();
 
     const homeLink = screen.getByRole("link", { name: /home/i });
@@ -21,47 +20,40 @@ describe("App component", () => {
 
     const aboutLink = screen.getByRole("link", { name: /about/i });
     expect(aboutLink).toBeInTheDocument();
-    });
+  });
 
-    it("should toggle the card image after each click", ()=>{
-      // mocking green,setGreen
-      
-      render(
-        <MemoryRouter>
-        <TopBar green={green} setGreen={setGreen}/>
+  it("should toggle the card image after each click", () => {
+    // mocking green,setGreen
+
+    render(
+      <MemoryRouter>
+        <TopBar green={green} setGreen={setGreen} />
       </MemoryRouter>
-        
-      );
-      const user = userEvent.setup();
-        const image = screen.getByRole('img')
-        expect(image.src.split('\\').pop().split('/').pop()).toMatch(/blue2.png/)
-        user.click(image)
-        screen.debug()
-        expect(image.src.split('\\').pop().split('/').pop()).toMatch(/astronaut.png/)
+    );
+    const user = userEvent.setup();
+    const image = screen.getByRole("img");
+    expect(image.src.split("\\").pop().split("/").pop()).toMatch(/blue2.png/);
+    user.click(image);
+    screen.debug();
+    expect(image.src.split("\\").pop().split("/").pop()).toMatch(
+      /astronaut.png/
+    );
+  });
 
+  it("should navigate to the about page when the link is clicked", () => {
+    const navigate = jest.fn();
+    const mockUseNavigate = jest.fn(() => navigate);
 
-    })
+    render(
+      <MemoryRouter>
+        <TopBar useNavigate={mockUseNavigate} />
+      </MemoryRouter>
+    );
 
-    it("should navigate to the about page when the link is clicked", () => {
-      const navigate = jest.fn();
-      const mockUseNavigate = jest.fn(() => navigate);
-  
-      render(
-        <MemoryRouter>
-          <TopBar useNavigate={mockUseNavigate} />
-        </MemoryRouter>
-      );
-  
-      const aboutLink = screen.getByRole("link", { name: /about/i });
-  
-      fireEvent.click(aboutLink);
-  
-      expect(navigate).toHaveBeenCalledWith("/about");
-    });
+    const aboutLink = screen.getByRole("link", { name: /about/i });
 
+    fireEvent.click(aboutLink);
 
-
-   
-  })
-
-
+    expect(navigate).toHaveBeenCalledWith("/about");
+  });
+});
